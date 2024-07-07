de3a86c016bbca1e1b19033b14ee3d49751f9472
apostorial <t.elouadrassi@gmail.com>
2024-07-07 16:15:43 +0100

    Initial Commit

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/WmBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.wemanity.wmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmBackendApplication.class, args);
+    }
+
+}
