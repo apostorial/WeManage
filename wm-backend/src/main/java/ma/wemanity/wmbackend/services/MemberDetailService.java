5b3a71276910d18b5107a105f16b4dc4d458ff99
apostorial <t.elouadrassi@gmail.com>
2024-07-09 00:09:36 +0100

    Used @AllArgsConstructor instead of actually writing constructors.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/services/MemberDetailService.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/services/MemberDetailService.java
@@ -1,5 +1,6 @@
 package ma.wemanity.wmbackend.services;
 
+import lombok.AllArgsConstructor;
 import ma.wemanity.wmbackend.entities.Member;
 import ma.wemanity.wmbackend.repositories.MemberRepository;
 import org.springframework.security.core.userdetails.User;
@@ -10,13 +11,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+@Service @AllArgsConstructor
 public class MemberDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
-
-    public MemberDetailService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
