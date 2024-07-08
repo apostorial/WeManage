09499142916ef9e9dcfaaecb05065f1b713c6288
apostorial <t.elouadrassi@gmail.com>
2024-07-08 23:45:14 +0100

    Added a BoardNotFoundException.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/BoardNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.wemanity.wmbackend.exceptions;
+
+public class BoardNotFoundException extends Exception {
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
