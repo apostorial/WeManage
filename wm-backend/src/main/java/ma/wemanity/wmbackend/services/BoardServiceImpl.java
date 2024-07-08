d061385d11c7724597e303120b7713d64a4ce9a7
apostorial <t.elouadrassi@gmail.com>
2024-07-08 23:49:24 +0100

    Implemented the service interface and wrote basic logic.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/services/BoardServiceImpl.java
@@ -0,0 +1,67 @@
+package ma.wemanity.wmbackend.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.wemanity.wmbackend.entities.Board;
+import ma.wemanity.wmbackend.entities.Member;
+import ma.wemanity.wmbackend.exceptions.BoardNotFoundException;
+import ma.wemanity.wmbackend.exceptions.ServiceException;
+import ma.wemanity.wmbackend.repositories.BoardRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service @AllArgsConstructor @Slf4j
+public class BoardServiceImpl implements BoardService {
+    private final BoardRepository boardRepository;
+
+    @Override
+    public Board createBoard(Board board) throws ServiceException {
+        try {
+            //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            //Member authenticatedUser = (Member) authentication.getPrincipal();
+            //board.setOwner(authenticatedUser);
+            return boardRepository.save(board);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to create board", e);
+        }
+    }
+
+    @Override
+    public Board updateBoard(Board board) throws ServiceException {
+        try {
+            if (!boardRepository.existsById(board.getId())) {
+                throw new BoardNotFoundException("Board not found with id: " + board.getId());
+            }
+            return boardRepository.save(board);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to update board", e);
+        }
+    }
+
+    @Override
+    public void deleteBoard(String id) throws ServiceException{
+        try {
+            if (!boardRepository.existsById(id)) {
+                throw new BoardNotFoundException("Board not found with id: " + id);
+            }
+            boardRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to delete board", e);
+        }
+    }
+
+    @Override
+    public Optional<Board> getBoard(Board board) throws ServiceException {
+        try {
+            if (!boardRepository.existsById(board.getId())) {
+                throw new BoardNotFoundException("Board not found with id: " + board.getId());
+            }
+            return boardRepository.findById(board.getId());
+        } catch (Exception e) {
+            throw new ServiceException("Failed to retrieve board", e);
+        }
+    }
+}
