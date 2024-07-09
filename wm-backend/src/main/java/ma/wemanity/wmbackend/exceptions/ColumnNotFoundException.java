87936201df0df137c7d5a93201beb18bf8703b9e
apostorial <t.elouadrassi@gmail.com>
2024-07-09 16:46:24 +0100

    Created the ColumnNotFoundException method.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/ColumnNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.wemanity.wmbackend.exceptions;
+
+public class ColumnNotFoundException extends Exception{
+    public ColumnNotFoundException(String message){
+        super(message);
+    }
+}
