11fdb650e9d42e0ba966e07c51ff82b8251cdb51
apostorial <t.elouadrassi@gmail.com>
2024-07-09 10:27:37 +0100

    Added the AuthenticationManager and refactored the securityFilterChain

--- wm-backend/src/main/java/ma/wemanity/wmbackend/config/SecurityConfig.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/config/SecurityConfig.java
@@ -1,49 +1,58 @@
 package ma.wemanity.wmbackend.config;
 
-import ma.wemanity.wmbackend.services.MemberDetailService;
+import lombok.AllArgsConstructor;
+import ma.wemanity.wmbackend.services.MemberDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration @EnableWebSecurity
-public class SecurityConfig {
-    private final MemberDetailService memberDetailService;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-    public SecurityConfig(MemberDetailService memberDetailService) {
-        this.memberDetailService = memberDetailService;
-    }
+@Configuration @AllArgsConstructor @EnableWebSecurity
+public class SecurityConfig {
+    private final MemberDetailsService memberDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/api/user/**").permitAll();
-                    registry.requestMatchers("/swagger-ui/index.html").hasAuthority("ADMIN");
-                    registry.anyRequest().authenticated();
-                })
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                .build();
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(registry -> {
+                registry.requestMatchers("/api/user/**").permitAll();
+                registry.requestMatchers("/swagger-ui/index.html").hasRole("ADMIN");
+                registry.anyRequest().authenticated();
+            })
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults()).build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(memberDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return memberDetailService;
+        return memberDetailsService;
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(memberDetailService);
+        daoAuthenticationProvider.setUserDetailsService(memberDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
