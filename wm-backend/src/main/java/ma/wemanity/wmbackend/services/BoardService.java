ee0adf1a31294fee6e48d2be7a7d86933afd1074
apostorial <t.elouadrassi@gmail.com>
2024-07-09 11:24:52 +0100

    Updated the logic for updating a board.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/services/BoardService.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/services/BoardService.java
@@ -1,14 +1,16 @@
 package ma.wemanity.wmbackend.services;
 
 import ma.wemanity.wmbackend.entities.Board;
+import ma.wemanity.wmbackend.entities.Member;
 import ma.wemanity.wmbackend.exceptions.BoardNotFoundException;
 import ma.wemanity.wmbackend.exceptions.ServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface BoardService {
     Board createBoard(Board board) throws ServiceException;
-    Board updateBoard(Board board) throws BoardNotFoundException, ServiceException;
+    Board updateBoard(String id, String name, String description, UserDetails authenticatedUser) throws BoardNotFoundException, ServiceException;
     void deleteBoard(String id) throws BoardNotFoundException, ServiceException;
     Optional<Board> getBoard(Board board) throws BoardNotFoundException, ServiceException;
 //    List<BoardDTO> getBoardsByUserId(Long userId) throws BoardNotFoundException, ServiceException;
