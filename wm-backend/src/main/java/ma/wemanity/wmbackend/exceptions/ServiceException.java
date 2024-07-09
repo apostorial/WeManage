c2cad3190b36196ffc73a7cef4cc5d9ec0b848c5
apostorial <t.elouadrassi@gmail.com>
2024-07-09 10:22:18 +0100

    Added an exception without a Throwable parameter.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/ServiceException.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/ServiceException.java
@@ -1,6 +1,9 @@
 package ma.wemanity.wmbackend.exceptions;
 
 public class ServiceException extends Exception {
+    public ServiceException(String message) {
+        super(message);
+    }
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
