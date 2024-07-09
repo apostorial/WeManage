2a955fcea768c3ec03ef0de34d9f075d94355f66
apostorial <t.elouadrassi@gmail.com>
2024-07-09 17:17:59 +0100

    Corrected the method name.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/services/ColumnService.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/services/ColumnService.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface ColumnService {
     Column createColumn(String boardId, String name) throws ServiceException;
     Column updateColumn(String id, String name, String description) throws ColumnNotFoundException, ServiceException;
-    void deleteBoard(String id) throws ColumnNotFoundException, ServiceException;
+    void deleteColumn(String id) throws ColumnNotFoundException, ServiceException;
     List<Column> getColumnsByBoard(Board board) throws ServiceException;
 }
