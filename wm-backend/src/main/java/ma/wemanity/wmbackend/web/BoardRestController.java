8179e238e0bdfaf087acd10af22388a4f3c2d69a
apostorial <t.elouadrassi@gmail.com>
2024-07-09 12:15:42 +0100

    Added the delete board method.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/web/BoardRestController.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/web/BoardRestController.java
@@ -2,14 +2,12 @@ package ma.wemanity.wmbackend.web;
 
 import lombok.AllArgsConstructor;
 import ma.wemanity.wmbackend.entities.Board;
-import ma.wemanity.wmbackend.entities.Member;
 import ma.wemanity.wmbackend.exceptions.BoardNotFoundException;
 import ma.wemanity.wmbackend.exceptions.ServiceException;
 import ma.wemanity.wmbackend.services.BoardService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,8 +32,8 @@ public class BoardRestController {
                                              Authentication authentication) {
         try {
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            Board updatedBoard = boardService.updateBoard(id, name, description, userDetails);
-            return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+            Board board = boardService.updateBoard(id, name, description, userDetails);
+            return new ResponseEntity<>(board, HttpStatus.OK);
         } catch (BoardNotFoundException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (ServiceException e) {
@@ -43,4 +41,17 @@ public class BoardRestController {
         }
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteBoard(@PathVariable("id") String id,
+                                            Authentication authentication) {
+        try {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            boardService.deleteBoard(id, userDetails);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (BoardNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (ServiceException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
