ab67fc52143d4807f990cc00971b29f8b012afec
apostorial <t.elouadrassi@gmail.com>
2024-07-09 13:11:02 +0100

    Added a findOwnerById method.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/BoardRepository.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/BoardRepository.java
@@ -3,5 +3,8 @@ package ma.wemanity.wmbackend.repositories;
 import ma.wemanity.wmbackend.entities.Board;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends MongoRepository<Board, String> {
+    List<Board> findByOwnerId(String id);
 }
