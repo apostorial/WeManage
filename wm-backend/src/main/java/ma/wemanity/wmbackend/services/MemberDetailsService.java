837d2b86e193c0a6f462a32c2f0d79fa9fa0567b
apostorial <t.elouadrassi@gmail.com>
2024-07-09 10:21:16 +0100

    Refactored the code to be compatible to Set<Role>.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/services/MemberDetailsService.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/services/MemberDetailsService.java
@@ -3,16 +3,20 @@ package ma.wemanity.wmbackend.services;
 import lombok.AllArgsConstructor;
 import ma.wemanity.wmbackend.entities.Member;
 import ma.wemanity.wmbackend.repositories.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service @AllArgsConstructor
-public class MemberDetailService implements UserDetailsService {
+public class MemberDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
@@ -20,13 +24,21 @@ public class MemberDetailService implements UserDetailsService {
         Optional<Member> user = memberRepository.findByUsername(username);
         if (user.isPresent()) {
             var member = user.get();
-            return User.builder()
-                    .username(member.getUsername())
-                    .password(member.getPassword())
-                    .roles(String.valueOf(member.getRole()))
-                    .build();
+            return new org.springframework.security.core.userdetails.User(
+                    member.getUsername(),
+                    member.getPassword(),
+                    member.getRole().stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                            .collect(Collectors.toList())
+            );
         } else {
             throw new UsernameNotFoundException(username);
         }
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Member member) {
+        return member.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
     }
 }
