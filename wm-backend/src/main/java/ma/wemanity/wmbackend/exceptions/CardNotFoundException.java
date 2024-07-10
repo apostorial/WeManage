db4e313352f7cc7a0b9d8dfbc723eec9dcac4de2
apostorial <t.elouadrassi@gmail.com>
2024-07-10 23:35:57 +0100

    Added this CardNotFoundException.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/CardNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.wemanity.wmbackend.exceptions;
+
+public class CardNotFoundException extends Exception {
+    public CardNotFoundException(String message) {
+        super(message);
+    }
+}
