392c27f46ee9c7f565693765d7f11681b77aad23
apostorial <t.elouadrassi@gmail.com>
2024-07-11 16:53:37 +0100

    Added CommentNotFoundException.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.wemanity.wmbackend.exceptions;
+
+public class CommentNotFoundException extends Exception {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+}
