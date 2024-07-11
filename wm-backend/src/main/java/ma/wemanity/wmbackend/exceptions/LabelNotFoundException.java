ba50956e91dd7403ef88d7c94f6a968f25e63c14
apostorial <t.elouadrassi@gmail.com>
2024-07-11 16:53:53 +0100

    Added LabelNotFoundException.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/exceptions/LabelNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.wemanity.wmbackend.exceptions;
+
+public class LabelNotFoundException extends Exception{
+    public LabelNotFoundException(String message) {
+        super(message);
+    }
+}
