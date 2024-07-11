69205a616b238c745dc29900bf7c26250a01fe7c
apostorial <t.elouadrassi@gmail.com>
2024-07-11 23:44:01 +0100

    Fixed some nesting issues.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/entities/Label.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/entities/Label.java
@@ -12,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -31,5 +32,18 @@ public class Label {
         return cards.stream()
                 .map(Card::getId)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Label label = (Label) o;
+        return Objects.equals(id, label.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
