c5b40ef0d8b09097735dbb45ba09e272ec367865
apostorial <t.elouadrassi@gmail.com>
2024-07-15 17:15:27 +0100

    Added a Card component that is inside the Columns.

---
+++ wm-frontend/src/components/Card.jsx
@@ -0,0 +1,13 @@
+// src/components/Card.jsx
+import React from 'react';
+import '../styles/Card.css';
+
+const Card = ({ card }) => {
+  return (
+    <div className="card">
+      <h4>{card.name}</h4>
+    </div>
+  );
+};
+
+export default Card;
