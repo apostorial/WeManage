d79678a53e5ad2a52c3effbf099b66c492125fe7
apostorial <t.elouadrassi@gmail.com>
2024-07-15 17:16:33 +0100

    Corrected redirection after login.

--- wm-frontend/src/App.jsx
+++ wm-frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import Login from './Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Sidebar from './components/Sidebar';
+import MainView from './components/MainView';
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<MainView />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
