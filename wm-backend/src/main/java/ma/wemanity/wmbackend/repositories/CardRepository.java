762f25f9e6a32afbe4da14b55ee9436426e5e033
apostorial <t.elouadrassi@gmail.com>
2024-07-18 15:55:33 +0100

    Removed the findByColumnId method since it doesn't display cards by their order in the database.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/CardRepository.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/CardRepository.java
@@ -3,8 +3,5 @@ package ma.wemanity.wmbackend.repositories;
 import ma.wemanity.wmbackend.entities.Card;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface CardRepository extends MongoRepository<Card, String> {
-    List<Card> findByColumnId(String id);
 }
