e4653519f63555f0f026519c20ad7c648df7314a
apostorial <t.elouadrassi@gmail.com>
2024-07-18 15:04:28 +0100

    Removed the findBoardById method since it doesn't display columns by their order in the database.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/ColumnRepository.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/ColumnRepository.java
@@ -3,8 +3,5 @@ package ma.wemanity.wmbackend.repositories;
 import ma.wemanity.wmbackend.entities.Column;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface ColumnRepository extends MongoRepository<Column, String> {
-    List<Column> findByBoardId(String id);
 }
