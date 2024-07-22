67c5adb3fa4225d6e43cff2f4b6444aaa549c2f7
apostorial <t.elouadrassi@gmail.com>
2024-07-23 00:58:28 +0100

    Corrected classes from class to className.

--- wm-frontend/src/components/Sidebar.jsx
+++ wm-frontend/src/components/Sidebar.jsx
@@ -84,66 +84,66 @@ const Sidebar = ({ boards, onBoardSelect, onAddBoard, onDeleteBoard }) => {
                 ))
               )}
               <a href="" className="board-option add-new" onClick={handleAddBoard}>
-                  <img src={boardAddIcon} alt="Board Add Icon" className="sidebar-icon" />
-                  <input
-                    type="text"
-                    value={newBoardName}
-                    onChange={(e) => setNewBoardName(e.target.value)}
-                    placeholder="Add a new board"
-                    onKeyDown={handleKeyDown}
-                  />
-                </a>
-              </div>
+                <img src={boardAddIcon} alt="Board Add Icon" className="sidebar-icon" />
+                <input
+                  type="text"
+                  value={newBoardName}
+                  onChange={(e) => setNewBoardName(e.target.value)}
+                  placeholder="Add a new board"
+                  onKeyDown={handleKeyDown}
+                />
+              </a>
             </div>
-            <a href="" class="sidebar-item">
+            </div>
+            <a href="" className="sidebar-item">
               <img src={calendarIcon} alt="Calendar Icon" className="sidebar-icon" />
               <span>Calendar</span>
             </a>
-            <a href="" class="sidebar-item">
+            <a href="" className="sidebar-item">
               <img src={reportIcon} alt="Report Icon" className="sidebar-icon" />
               <span>Reports</span>
             </a>
-            <a href="" class="sidebar-item">
+            <a href="" className="sidebar-item">
               <img src={statisticIcon} alt="Statistic Icon" className="sidebar-icon" />
               <span>Statistics</span>
             </a>
           </div>
         </div>
       </div>
-      <div class="team-section">
-				<div class="main-text">Team</div>
-				<div class="sidebar-list">
-					<a href="" class="sidebar-item">
+      <div className="team-section">
+				<div className="main-text">Team</div>
+				<div className="sidebar-list">
+					<a href="" className="sidebar-item">
             <img src={membersIcon} alt="Members Icon" className="sidebar-icon" />						
 						<span>Members</span>
 					</a>
-					<a href="" class="sidebar-item">
+					<a href="" className="sidebar-item">
             <img src={tasksIcon} alt="Tasks Icon" className="sidebar-icon" />
 						<span>Tasks</span>
 					</a>
-					<a href="" class="sidebar-item">
+					<a href="" className="sidebar-item">
             <img src={inboxIcon} alt="Inbox Icon" className="sidebar-icon" />
 						<span>Inbox</span>
 					</a>
 				</div>
 			</div>
-      <div class="info-section">
-        <div class="main-text">Information</div>
-        <div class="sidebar-list">
-          <a href="" class="sidebar-item">
+      <div className="info-section">
+        <div className="main-text">Information</div>
+        <div className="sidebar-list">
+          <a href="" className="sidebar-item">
           <img src={documentationIcon} alt="Inbox Icon" className="sidebar-icon" />
             <span>Documentation</span>
           </a>
-          <a href="" class="sidebar-item">
+          <a href="" className="sidebar-item">
             <img src={settingsIcon} alt="Settings Icon" className="sidebar-icon" />
             <span>Settings</span>
           </a>
-          <a href="" class="sidebar-item-logout" onClick={handleLogout}>
-          <svg className='sidebar-icon' width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.41699 6.29998C7.67533 3.29998 9.21699 2.07498 12.592 2.07498H12.7003C16.4253 2.07498 17.917 3.56665 17.917 7.29165V12.725C17.917 16.45 16.4253 17.9416 12.7003 17.9416H12.592C9.24199 17.9416 7.70033 16.7333 7.42533 13.7833" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M12.4999 10H3.0166" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M4.87467 7.20834L2.08301 10L4.87467 12.7917" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>		
+          <a href="" className="sidebar-item-logout" onClick={handleLogout}>
+            <svg className='sidebar-icon' width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M7.41699 6.29998C7.67533 3.29998 9.21699 2.07498 12.592 2.07498H12.7003C16.4253 2.07498 17.917 3.56665 17.917 7.29165V12.725C17.917 16.45 16.4253 17.9416 12.7003 17.9416H12.592C9.24199 17.9416 7.70033 16.7333 7.42533 13.7833" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M12.4999 10H3.0166" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M4.87467 7.20834L2.08301 10L4.87467 12.7917" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>		
             <span>Logout</span>
           </a>
         </div>
