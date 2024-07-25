70e17ecbd816fff4b8a0ca2230e8f6d0c45badc5
apostorial <t.elouadrassi@gmail.com>
2024-07-25 17:15:38 +0100

    Created new success and failure handlers.

---
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/config/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,20 @@
+package ma.wemanity.wmbackend.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component @AllArgsConstructor
+public class OAuth2AuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
