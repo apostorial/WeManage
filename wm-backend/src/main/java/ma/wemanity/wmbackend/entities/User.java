03f56982ae970b49fd0382f6a2d3251befca620f
apostorial <t.elouadrassi@gmail.com>
2024-07-25 17:12:32 +0100

    Removed the username and password field and replaced them with name, googleId and email fields, and added a constructor for them.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/entities/User.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/entities/User.java
@@ -6,25 +6,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ma.wemanity.wmbackend.enums.Role;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Document @Data @NoArgsConstructor @AllArgsConstructor
-public class User implements UserDetails {
+public class User{
     @Id
     private String id;
-    @NotNull @Indexed(unique = true)
-    private String username;
-    @NotNull
-    private String password;
+    private String name;
+    private String googleId;
+    private String email;
     @NotNull
     private Set<Role> role = new HashSet<>();
     @DBRef
@@ -34,28 +27,10 @@ public class User implements UserDetails {
     @DBRef
     private Set<Comment> authorOf = new HashSet<>();
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+    public User(String name, String email, String googleId) {
+        this.name = name;
+        this.email = email;
+        this.googleId = googleId;
+        this.role.add(Role.USER);
     }
 }
