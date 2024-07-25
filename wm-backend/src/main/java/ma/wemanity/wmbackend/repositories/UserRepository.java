1d7007333afcd11d908385d3a44c3dbfb30f47a0
apostorial <t.elouadrassi@gmail.com>
2024-07-25 17:12:51 +0100

    Added a findByEmail method to the repository.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/UserRepository.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByGoogleId(String googleId);
+    Optional<User> findByEmail(String email);
 }
