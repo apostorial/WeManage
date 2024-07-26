d13f4f05c7d8180432bd919efce8c99beadf220a
apostorial <t.elouadrassi@gmail.com>
2024-07-26 19:19:45 +0100

    Modified the App component to protect some routes that need authentication.

--- wm-frontend/src/App.jsx
+++ wm-frontend/src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import MainView from './components/MainView';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/main" element={<MainView />} />
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/main" 
+          element={
+            <ProtectedRoute>
+              <MainView />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
