b1394a1d2e9f0fcb2ed5badd5f7b17eee3ac3d2d
apostorial <t.elouadrassi@gmail.com>
2024-08-05 16:57:23 +0100

    Included the new CardFormPopup and removed the add card input.

--- wm-frontend/src/components/Column.jsx
+++ wm-frontend/src/components/Column.jsx
@@ -2,35 +2,60 @@ import React, { useState, useEffect } from 'react';
 import axios from '../axios-config.js';
 import { Droppable, Draggable } from '@hello-pangea/dnd';
 import Card from './Card';
+import CardFormPopup from './CardFormPopup';
 import '../styles/Column.css';
 import deleteIcon from '../assets/delete.svg'
 import divider from '../assets/divider.svg'
 import EmptyState from '../assets/empty_state.svg?react';
+import AddIcon from '../assets/add.svg?react';
 
-const Column = ({ column, onColumnNameUpdate, onDeleteColumn, onAddCard, onUpdateCard, onDeleteCard, onEditColumn }) => {
-  const [isAddingCard, setIsAddingCard] = useState(false);
-  const [newCardName, setNewCardName] = useState('');
+const Column = ({ column, onColumnNameUpdate, onDeleteColumn, onEditColumn, onAddCard, onUpdateCard}) => {
   const [columnName, setColumnName] = useState(column.name);
+  const [isAddCardPopupOpen, setIsAddCardPopupOpen] = useState(false);
+  const [isEditCardPopupOpen, setIsEditCardPopupOpen] = useState(false);
+  const [cardToEdit, setCardToEdit] = useState(null);
+  const [cards, setCards] = useState(column.cards || []);
 
   useEffect(() => {
     setColumnName(column.name);
-  }, [column.name]);
+    setCards(column.cards || []);
+  }, [column.name, column.cards]);
 
-  const handleAddCard = async (e) => {
-    e.preventDefault();
-    if (!newCardName.trim()) return;
+  const addCard = () => {
+    setIsAddCardPopupOpen(true);
+  };
 
-    try {
-      const response = await axios.post('/api/cards/create', new URLSearchParams({
-        name: newCardName,
-        columnId: column.id,
-      }));
-      onAddCard(column.id, response.data);
-      setNewCardName('');
-      setIsAddingCard(false);
-    } catch (error) {
-      console.error('Error creating card:', error);
-    }
+  const handleAddCard = (newCard) => {
+    setCards(prevCards => [...prevCards, { ...newCard }]);
+    onAddCard(column.id, newCard);
+  };
+
+  const closeAddCardPopup = () => {
+  setIsAddCardPopupOpen(false);
+  };
+
+  const editCard = (card) => {
+    setCardToEdit(card);
+    setIsEditCardPopupOpen(true);
+  };
+  
+  const closeEditCardPopup = () => {
+    setIsEditCardPopupOpen(false);
+    setCardToEdit(null);
+  };
+
+  const handleUpdateCard = (updatedCard) => {
+    setCards(prevCards =>
+        prevCards.map(card =>
+            card.id === updatedCard.id ? updatedCard : card
+        )
+    );
+    onUpdateCard(column.id, updatedCard);
+    closeEditCardPopup();
+};
+
+  const handleDeleteCard = (cardId) => {
+    setCards(cards.filter(card => card.id !== cardId));
   };
 
   const handleDeleteColumn = async () => {
@@ -59,7 +84,7 @@ const Column = ({ column, onColumnNameUpdate, onDeleteColumn, onAddCard, onUpdat
           </div>
         </div>
         <div className='add-icon-parent'>
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg onClick={addCard} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 8H12" stroke="#EBF7FB" strokeLinecap="round" strokeLinejoin="round"/>
           <path d="M8 12V4" stroke="#EBF7FB" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
@@ -91,8 +116,8 @@ const Column = ({ column, onColumnNameUpdate, onDeleteColumn, onAddCard, onUpdat
                       <Card
                         key={card.id}
                         card={card}
-                        onUpdate={(updatedCard) => onUpdateCard(column.id, updatedCard)}
-                        onDelete={() => onDeleteCard(column.id, card.id)}
+                        onDeleteCard={handleDeleteCard}
+                        onEditCard={editCard}
                       />
                     </div>
                   )}
@@ -104,24 +129,19 @@ const Column = ({ column, onColumnNameUpdate, onDeleteColumn, onAddCard, onUpdat
         )}
       </Droppable>
       <img src={divider} alt="Divider" className="footer-divider" />
-      <div className="add-new-button" id="addNewButton">
-        {/* <img src={addIcon} alt="Add Icon" className="add-icon" /> */}
-				<div className="add-new">
-          <a href="" className="board-option add-new" onClick={handleAddCard}>
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4 8H12" stroke="#EBF7FB" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M8 12V4" stroke="#EBF7FB" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-            <input
-              type="text"
-              value={newCardName}
-              onChange={(e) => setNewCardName(e.target.value)}
-              placeholder="Add a new card"
-              onKeyDown={handleKeyDown}
-            />
-          </a>
-        </div>
+      <div className="add-new-button" id="addNewButton" onClick={addCard}>
+        <AddIcon className='add-column-icon' />
+        <div className="add-new">Add new</div>
 			</div>
+
+      {(isAddCardPopupOpen || isEditCardPopupOpen) && (
+        <CardFormPopup
+          onClose={isAddCardPopupOpen ? closeAddCardPopup : closeEditCardPopup}
+          onSubmit={isAddCardPopupOpen ? handleAddCard : handleUpdateCard}
+          columnId={column.id}
+          editCard={cardToEdit}
+        />
+      )}
     </div>
   );
 };
