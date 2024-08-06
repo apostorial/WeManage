87feeb6ac9cda1a5228ebb4a8c8bcc7a10a6ca48
apostorial <t.elouadrassi@gmail.com>
2024-08-06 13:18:29 +0100

    Added a findByOwnerId method.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/LabelRepository.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/repositories/LabelRepository.java
@@ -1,7 +1,11 @@
 package ma.wemanity.wmbackend.repositories;
 
+import ma.wemanity.wmbackend.entities.Board;
 import ma.wemanity.wmbackend.entities.Label;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface LabelRepository extends MongoRepository<Label, String> {
+    List<Label> findByOwnerId(String id);
 }
