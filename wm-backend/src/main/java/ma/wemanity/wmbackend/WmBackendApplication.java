a9c16572f18e5b478380ace115162628ca4e1e12
apostorial <t.elouadrassi@gmail.com>
2024-08-13 00:27:54 +0100

    Set up .env.

--- wm-backend/src/main/java/ma/wemanity/wmbackend/WmBackendApplication.java
+++ wm-backend/src/main/java/ma/wemanity/wmbackend/WmBackendApplication.java
@@ -1,5 +1,6 @@
 package ma.wemanity.wmbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WmBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.data.mongodb.host", dotenv.get("MONGODB_HOST"));
+        System.setProperty("spring.data.mongodb.port", dotenv.get("MONGODB_PORT"));
+        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGODB_DATABASE"));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-id", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("spring.security.oauth2.client.registration.google.client-secret", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("spring.security.oauth2.client.registration.google.scope", dotenv.get("GOOGLE_SCOPE"));
+
         SpringApplication.run(WmBackendApplication.class, args);
     }
 
